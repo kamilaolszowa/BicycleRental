4c2e9c805c10bf79c78453cd2851333c940368f0
kamila.olszowa <kamila.olszowa@hotmail.com>
2022-01-02 22:12:53 +0100

    serializing relation brands-bicycles changed

--- bicycles/serializers.py
+++ bicycles/serializers.py
@@ -4,7 +4,15 @@ from rest_framework import serializers
 from bicycles.models import Bicycle, Brand
 
 
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ['id', 'name']
+
+
 class BicycleSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+
     class Meta:
         model = Bicycle
         fields = ['id', 'model', 'brand', 'daily_rate']
